62fdc60acb2c32b427a591c8e7e4790af9183449
sascha <s.lumma@gmail.com>
2026-07-05 21:44:13 +0200

    Create initial structure

--- source/app/source/decoder.cpp
+++ source/app/source/decoder.cpp
@@ -15,7 +15,7 @@
 
 int main(int argc, char **argv)
 {
-    auto cmd = TCLAP::CmdLine("ticket-decoder", ' ', "v0.20.2");
+    auto cmd = TCLAP::CmdLine("ticket-decoder", ' ', "v0.21.0");
     auto const verboseArg = TCLAP::SwitchArg(
         "v", "verbose",
         "More verbose debug logging",
